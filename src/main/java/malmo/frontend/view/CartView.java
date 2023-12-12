707d5f08ddeefc749062a323e8d5d7fa9318fdd2
kellyvasss <99kelvas@gafe.molndal.se>
2023-12-12 19:33:49 +0100

    -

--- src/main/java/malmo/frontend/view/CartView.java
+++ src/main/java/malmo/frontend/view/CartView.java
@@ -1,13 +1,17 @@
 package malmo.frontend.view;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import malmo.frontend.dto.Article;
+import malmo.frontend.api.CartAPI;
 import malmo.frontend.dto.Cart;
-import malmo.frontend.dto.CartItem;
+import malmo.frontend.view.util.button.CartButton;
 import malmo.frontend.view.layout.UserLayout;
 
 @Route(value = "cart", layout = UserLayout.class)
@@ -30,33 +34,49 @@ public class CartView extends VerticalLayout {
         grid.addColumn(cart -> cart.getArticle().getCost() * cart.getQuantity()).setHeader("Pris");
 
         grid.addComponentColumn(cart -> {
-            Icon plus = new Icon(VaadinIcon.PLUS);
-            plus.addClickListener(click -> {
-                // Uppdatera antal med en
-            });
-            return plus;
-        });
+            // uppdatera med en
+            CartButton add = new CartButton(VaadinIcon.PLUS, cart.getArticle().getName(), false);
+            return add;
+        }).setWidth("150px").setFlexGrow(0);
         grid.addComponentColumn(cart -> {
-            Icon minus = new Icon(VaadinIcon.MINUS);
-            minus.addClickListener(click -> {
-                // Ta bort en från kundkorg
-            });
+            // ta bort en
+            CartButton minus = new CartButton(VaadinIcon.MINUS, cart.getArticle().getName(), cart.getQuantity());
             return minus;
-        })
 
-        grid.addComponentColumn(cart -> {
-            Icon trash = new Icon(VaadinIcon.TRASH);
-            trash.addClickListener(click -> {
-                // Ta bort alla
-
-            });
-            return trash;
         }).setWidth("150px").setFlexGrow(0);
 
+        grid.addComponentColumn(cart -> {
+            // ta bort alla
+            CartButton delete = new CartButton(VaadinIcon.TRASH, cart.getArticle().getName(), true);
+            return delete;
+        }).setWidth("150px").setFlexGrow(0);
 
-
-        grid.asSingleSelect().addValueChangeListener(marked -> article = marked.getValue());
     }
+    private static void deleteArticleFromCart(String articleName) {
+        CartAPI.deleteItemFromCart(articleName);
+    }
+
+    private void showConfirmDialog(String articleName) {
+        Dialog dialog = new Dialog();
+        dialog.add(createDialogContent(dialog, articleName));
+
+    }
+    private static VerticalLayout createDialogContent(Dialog dialog, String articleName) {
+        H2 header = new H2(String.format("Radera %s?", articleName));
+        header.getStyle().set("margin", "var(--lumo-space-m) 0")
+                .set("font-size", "1.5em").set("font-weight", "bold");
+        
+        Button btnCancel = new Button("Avbryt");
+        Button btnDelete = new Button("Radera");
+        btnDelete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        btnCancel.addClickListener(e -> dialog.close());
+        btnDelete.addClickListener(e -> {
+            deleteArticleFromCart(articleName);
+            dialog.close();
+        });
+        return new VerticalLayout(header, btnCancel, btnDelete);
+    }
+
 
 
 }
---
+++ src/main/java/malmo/frontend/view/util/button/CartButton.java
@@ -0,0 +1,65 @@
+package malmo.frontend.view.util.button;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import malmo.frontend.api.CartAPI;
+import malmo.frontend.dto.CartItem;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+
+public class CartButton extends Button {
+    public CartButton(VaadinIcon vaadinIcon, String articleName, boolean delete) {
+        Icon icon = new Icon(vaadinIcon);
+        setIcon(icon);
+        CartItem cartItem = new CartItem(articleName, 1);
+        addClickListener(e -> {
+            if (!delete) {
+                try {
+                    CartAPI.updateCart(cartItem);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            else {
+                try {
+                    CartAPI.deleteItemFromCart(articleName);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+        });
+    }
+
+    public CartButton(VaadinIcon vaadinIcon, String articleName, int oldQuantity) {
+        Icon icon = new Icon(vaadinIcon);
+        setIcon(icon);
+        if (oldQuantity - 1 == 0) {
+            addClickListener(click ->
+            {
+                try {
+                    CartAPI.deleteItemFromCart(articleName);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        else {
+            CartItem cartItem = new CartItem(articleName, -1);
+            addClickListener(click -> {
+                try {
+                    CartAPI.updateCart(cartItem);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+}
